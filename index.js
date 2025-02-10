d4bd053ab8c5e5cb557cb5e0806270ff06849269
Hugo_古学冬 <hokdung@outlook.com>
2025-02-10 16:37:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: 0BSD
 
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+// const doh = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://9.9.9.11/dns-query'
+// const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const dohjson = 'https://9.9.9.11/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 const r404 = new Response(null, {status: 404});
